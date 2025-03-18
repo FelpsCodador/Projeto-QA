33e9c1b889c17234767cddfc7ad952bc74b5ba6f
FelpsCodador <118644937+FelpsCodador@users.noreply.github.com>
2025-03-18 13:48:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const gameArea = document.getElementById('gameArea');
+const player = document.getElementById('player');
+const scoreDisplay = document.getElementById('score');
+const livesDisplay = document.getElementById('lives');
+
+var x = 0;
+var y = 0;
